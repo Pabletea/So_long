13cbf1cc5e6b7dc0eaa8d75d81d430c9f585ad05
Pablo Alonso Garcia <pabalons@c3r9s4.42madrid.com>
2025-02-12 17:24:45 +0100

    move left and move right done

--- player_movement.c
+++ player_movement.c
@@ -6,7 +6,7 @@
 /*   By: pabalons <pabalons@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:18:47 by pabalons          #+#    #+#             */
-/*   Updated: 2025/02/12 17:21:06 by pabalons         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:24:11 by pabalons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ void	move_up(t_map *map)
 
 		// Mover al jugador hacia arriba
 		y--;
+		map->moves++;
 
 		print_moves(map);
 
@@ -92,6 +93,7 @@ void	move_down(t_map *map)
 			}
 		}
 		y++;
+		map->moves++;
 		print_moves(map);
 		mlx_image_to_window(map->mlx, map->img.player_down_basic1, x * PXL_IMG, y * PXL_IMG);
 		map->array[y][x] = 'P';
@@ -104,11 +106,9 @@ void	move_down(t_map *map)
 
 void	move_left(t_map *map)
 {
-	int	x;
-	int	y;
+	int	x = map->player.x;
+	int	y = map->player.y;
 
-	x = map->player.x;
-	y = map->player.y;
 	if (x > 0 && map->array[y][x - 1] != '1')
 	{
 		if (map->array[y][x - 1] == 'C')
@@ -117,29 +117,48 @@ void	move_left(t_map *map)
 			if (map->c == 0)
 				map->exit = 1; // Se desbloquea la salida
 		}
-		// move_resume(map, x, y, LEFT);
-		if (map->array[y][x - 1] == 'E' && (map->c != 0 || map->exit == 1))
-			exit_map(map);
-		map->moves++;
-        mlx_image_to_window(map->mlx, map->img.empty, x * PXL_IMG, y * PXL_IMG);
-		map->array[y][x] = '0';
+
+		// Si el jugador está sobre la salida, debe restaurarla antes de moverse
+		if (x == map->exit_x && y == map->exit_y)
+		{
+			// Restaurar la salida en la posición actual
+			mlx_image_to_window(map->mlx, map->img.exit, x * PXL_IMG, y * PXL_IMG);
+			map->array[y][x] = 'E';
+		}
+		else 
+		{
+			if (map->array[y][x - 1] == 'E' && map->exit == 1)
+				exit_map(map);
+			else
+			{
+				// Actualizar la posición anterior a vacío solo si no es la salida
+				if (map->array[y][x] != 'E')
+					map->array[y][x] = '0';
+
+				// Dibujar la casilla vacía
+				mlx_image_to_window(map->mlx, map->img.empty, x * PXL_IMG, y * PXL_IMG);
+			}
+		}
+
+		// Mover al jugador hacia la izquierda
 		x--;
+		map->moves++;
 		print_moves(map);
-        mlx_image_to_window(map->mlx, map->img.empty, x * PXL_IMG, y * PXL_IMG);
-        mlx_image_to_window(map->mlx, map->img.player_left1, x * PXL_IMG, y * PXL_IMG);
+
+		// Dibujar la nueva posición del jugador
+		mlx_image_to_window(map->mlx, map->img.player_left1, x * PXL_IMG, y * PXL_IMG);
 		map->array[y][x] = 'P';
-		map->player.y = y;
+		map->player.x = x;
 	}
 }
 
+
 void	move_right(t_map *map)
 {
-	int	x;
-	int	y;
+	int	x = map->player.x;
+	int	y = map->player.y;
 
-	x = map->player.x;
-	y = map->player.y;
-	if (x > 0 && map->array[y][x + 1] != '1')
+	if (x < map->x - 1 && map->array[y][x + 1] != '1')
 	{
 		if (map->array[y][x + 1] == 'C')
 		{
@@ -147,18 +166,38 @@ void	move_right(t_map *map)
 			if (map->c == 0)
 				map->exit = 1; // Se desbloquea la salida
 		}
-		// move_resume(map, x, y, RIGHT);
-		if (map->array[y][x + 1] == 'E' && (map->c != 0 || map->exit == 1))
-			exit_map(map);
-		map->moves++;
-        mlx_image_to_window(map->mlx, map->img.empty, x * PXL_IMG, y * PXL_IMG);
-		map->array[y][x] = '0';
+
+		// Si el jugador está sobre la salida, debe restaurarla antes de moverse
+		if (x == map->exit_x && y == map->exit_y)
+		{
+			// Restaurar la salida en la posición actual
+			mlx_image_to_window(map->mlx, map->img.exit, x * PXL_IMG, y * PXL_IMG);
+			map->array[y][x] = 'E';
+		}
+		else 
+		{
+			if (map->array[y][x + 1] == 'E' && map->exit == 1)
+				exit_map(map);
+			else
+			{
+				// Actualizar la posición anterior a vacío solo si no es la salida
+				if (map->array[y][x] != 'E')
+					map->array[y][x] = '0';
+
+				// Dibujar la casilla vacía
+				mlx_image_to_window(map->mlx, map->img.empty, x * PXL_IMG, y * PXL_IMG);
+			}
+		}
+
+		// Mover al jugador hacia la derecha
 		x++;
+		map->moves++;
 		print_moves(map);
-        mlx_image_to_window(map->mlx, map->img.empty, x * PXL_IMG, y * PXL_IMG);
-        mlx_image_to_window(map->mlx, map->img.player_right1, x * PXL_IMG, y * PXL_IMG);
+
+		// Dibujar la nueva posición del jugador
+		mlx_image_to_window(map->mlx, map->img.player_right1, x * PXL_IMG, y * PXL_IMG);
 		map->array[y][x] = 'P';
-		map->player.y = y;
+		map->player.x = x;
 	}
 }
 
